309a57408a1eb36ef5cb0928adea53d419f2df5c
Thiagão <thiago.vazss@gmail.com>
2022-09-29 10:20:51 -0300

    fix the homepage basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import './App.css';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import CreateChar from './components/CreateChar';
 import Home from './components/Home';
-import MainEvent from './components/MainEvent';
+import Fight from './components/Fight';
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter> 
+    <BrowserRouter basename='https://thiagaoz.github.io/mainevent/'> 
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='createchar' element={(<CreateChar />)}></Route>
-        <Route path='mainevent' element={<MainEvent/>}></Route>
+        <Route path='fight' element={<Fight/>}></Route>
       </Routes>
     </BrowserRouter>
     </div>
