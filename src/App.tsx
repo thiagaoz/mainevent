46a90a1cc870c020dae612613e7758199ef4a959
Thiagão <thiago.vazss@gmail.com>
2022-09-29 11:02:44 -0300

    Fixed Router problem: BrowserRouter => HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import {HashRouter, Routes, Route, Navigate} from 'react-router-dom';
 import CreateChar from './components/CreateChar';
 import Home from './components/Home';
 import Fight from './components/Fight';
@@ -7,13 +7,13 @@ import Fight from './components/Fight';
 function App() {
   return (
     <div className="App">
-    <BrowserRouter > 
+    <HashRouter > 
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='createchar' element={(<CreateChar />)}></Route>
         <Route path='fight' element={<Fight/>}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </div>
   );
 }
